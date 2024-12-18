b8694b36ed78c149dee110d0570c4b19d6e227dc
MuhammadHakim33 <mhmd.hakim53@gmail.com>
2024-12-18 21:39:06 +0700

    video 33

--- praktikum-laravel-parsinta-breeze/app/Http/Controllers/StoreController.php
+++ praktikum-laravel-parsinta-breeze/app/Http/Controllers/StoreController.php
@@ -6,7 +6,9 @@ use App\Models\Store;
 use App\Enums\StoreStatus;
 use Illuminate\Http\Request;
 use App\Http\Requests\StoreRequest;
+use App\Mail\StorePublished;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
@@ -29,6 +31,8 @@ class StoreController extends Controller
     {
         $store->status = StoreStatus::ACTIVE;
         $store->save();
+
+        Mail::to($store->user)->send(new StorePublished($store));
 
         return back();
     }
